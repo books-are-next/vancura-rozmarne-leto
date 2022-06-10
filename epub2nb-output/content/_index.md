67863ab9562ff965d830f83bfca4399306644786
jan-martinek <jan-martinek@users.noreply.github.com>
2022-06-10 18:04:48 +0000

    Deploying to docs from @ books-are-next/vancura-rozmarne-leto@90cff4ff1159cfddfd22cfb18b982bf031ba903e 🚀

--- epub2nb-output/content/_index.md
+++ epub2nb-output/content/_index.md
@@ -2,4 +2,8 @@
 contentType: prose
 ---
 
+<section>
+
 ![obalka_rozmarne_leto2.jpg](./resources/obalka_rozmarne_leto2_fmt.jpeg)
+
+</section>
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-d4e0cc8';
+const CACHE = 'cache-90cff4f';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
